0bb60f2c561300f2bbd0d03518f8184ab02b2b9e
Alberto Garcia <albertopaul3@icloud.com>
2023-08-23 18:29:25 -0600

    Move 'RemoteFeedItem' to its own file

--- EssentialFeed/EssentialFeed/Feed API/FeedItemsMapper.swift
+++ EssentialFeed/EssentialFeed/Feed API/FeedItemsMapper.swift
@@ -7,13 +7,6 @@
 
 import Foundation
 
-struct RemoteFeedItem: Decodable {
-    let id: UUID
-    let description: String?
-    let location: String?
-    let image: URL
-}
-
 enum FeedItemsMapper {
     private struct Root: Decodable {
         let items: [RemoteFeedItem]
