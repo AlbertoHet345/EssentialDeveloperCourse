af97475e17570633479536dcc788c39911600c64
Alberto Garcia <albertopaul3@icloud.com>
2023-10-20 18:48:40 -0600

    'CoreDataFeedStore.retrieve' delivers empty on empty cache

--- EssentialFeed/EssentialFeed/Feed Cache/CoreDataFeedStore.swift
+++ EssentialFeed/EssentialFeed/Feed Cache/CoreDataFeedStore.swift
@@ -8,79 +8,17 @@
 import CoreData
 
 final public class CoreDataFeedStore: FeedStore {
-    public static let modelName = "FeedStore"
-    public static let model = NSManagedObjectModel(name: modelName, in: Bundle(for: CoreDataFeedStore.self))
-
-    private let container: NSPersistentContainer
-    private let context: NSManagedObjectContext
-
-    public struct ModelNotFound: Error {
-        public let modelName: String
-    }
-
-    public init(storeURL: URL) throws {
-        guard let model = CoreDataFeedStore.model else {
-            throw ModelNotFound(modelName: CoreDataFeedStore.modelName)
-        }
-
-        container = try NSPersistentContainer.load(
-            name: CoreDataFeedStore.modelName,
-            model: model,
-            url: storeURL
-        )
-        context = container.newBackgroundContext()
-    }
-
-    deinit {
-        cleanUpReferencesToPersistentStores()
-    }
-
-    private func cleanUpReferencesToPersistentStores() {
-        context.performAndWait {
-            let coordinator = self.container.persistentStoreCoordinator
-            try? coordinator.persistentStores.forEach(coordinator.remove)
-        }
-    }
+    public init() {}
     
     public func retrieve(completion: @escaping RetrievalCompletion) {
-        perform { context in
-            completion(Result {
-                try ManagedCache.find(in: context).map {
-                    CachedFeed(feed: $0.localFeed, timestamp: $0.timestamp)
-                }
-            })
-        }
-    }
-
-    public func insert(_ feed: [LocalFeedImage], timestamp: Date, completion: @escaping InsertionCompletion) {
-        perform { context in
-            do {
-                let cache = try ManagedCache.newUniqueInstance(in: context)
-                cache.timestamp = timestamp
-                cache.feed = ManagedFeedImage.images(from: feed, in: context)
-                
-                try context.save()
-                completion(.success(()))
-            } catch {
-                context.rollback()
-                completion(.failure(error))
-            }
-        }
+        completion(.success(nil))
     }
     
     public func deleteCachedFeed(completion: @escaping DeletionCompletion) {
-        perform { context in
-            completion(Result {
-                try ManagedCache
-                    .find(in: context)
-                    .map(context.delete)
-                    .map(context.save)
-            })
-        }
+        
     }
     
-    private func perform(_ action: @escaping (NSManagedObjectContext) -> Void) {
-        let context = self.context
-        context.perform { action(context) }
+    public func insert(_ feed: [LocalFeedImage], timestamp: Date, completion: @escaping InsertionCompletion) {
+        
     }
 }
--- EssentialFeed/EssentialFeedTests/Feed Cache/CoreDataFeedStoreTests.swift
+++ EssentialFeed/EssentialFeedTests/Feed Cache/CoreDataFeedStoreTests.swift
@@ -6,10 +6,13 @@
 //
 
 import XCTest
+import EssentialFeed
 
 class CoreDataFeedStoreTests: XCTestCase, FeedStoreSpecs {
     func test_retrieve_deliversEmptyOnEmptyCache() {
+        let sut = makeSUT()
         
+        assertThatRetrieveDeliversEmptyOnEmptyCache(on: sut)
     }
     
     func test_retrieve_hasNoSideEffectsOnEmptyCache() {
@@ -56,5 +59,12 @@ class CoreDataFeedStoreTests: XCTestCase, FeedStoreSpecs {
         
     }
     
+    // MARK: - Helpers
+    
+    private func makeSUT(file: StaticString = #filePath, line: UInt = #line) -> FeedStore {
+        let sut = CoreDataFeedStore()
+        trackForMemoryLeaks(sut, file: file, line: line)
+        return sut
+    }
     
 }
