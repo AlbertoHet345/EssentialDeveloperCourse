1f5b91c38748713f18db57437f8dbed30d905bf8
Alberto Garcia <albertopaul3@icloud.com>
2023-11-14 19:55:03 -0600

    Render loaded feed on successfull load completion

---
+++ EssentialFeed/EssentialFeediOS/FeedImageCell.swift
@@ -0,0 +1,14 @@
+//
+//  FeedImageCell.swift
+//  EssentialFeediOS
+//
+//  Created by Alberto Garcia Paul on 14/11/23.
+//
+
+import UIKit
+
+public final class FeedImageCell: UITableViewCell {
+    public let locationContainer = UIView()
+    public let locationLabel = UILabel()
+    public let descriptionLabel = UILabel()
+}
