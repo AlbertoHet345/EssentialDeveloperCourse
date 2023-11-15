fee32e07393a2de11f39ff2382a2f434ea65efc1
Alberto Garcia <albertopaul3@icloud.com>
2023-11-15 16:28:02 -0600

    Extract UIButton test helper to a separate file

--- EssentialFeed/EssentialFeediOSTests/FeedViewControllerTests.swift
+++ EssentialFeed/EssentialFeediOSTests/FeedViewControllerTests.swift
@@ -415,15 +415,7 @@ private extension FeedViewController {
     }
 }
 
-private extension UIButton {
-    func simulateTap() {
-        allTargets.forEach { target in
-            actions(forTarget: target, forControlEvent: .touchUpInside)?.forEach {
-                (target as NSObject).perform(Selector($0))
-            }
-        }
-    }
-}
+
 
 
 
