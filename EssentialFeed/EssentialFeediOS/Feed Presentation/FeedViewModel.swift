995e68dd4b24b442820e698287a9b73fda683d76
Alberto Garcia <albertopaul3@icloud.com>
2023-11-22 16:42:18 -0600

    Move ViewModels to separate files

---
+++ EssentialFeed/EssentialFeediOS/Feed Presentation/FeedViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  FeedViewModel.swift
+//  EssentialFeediOS
+//
+//  Created by Alberto Garcia Paul on 22/11/23.
+//
+
+import EssentialFeed
+
+struct FeedViewModel {
+    let feed: [FeedImage]
+}
