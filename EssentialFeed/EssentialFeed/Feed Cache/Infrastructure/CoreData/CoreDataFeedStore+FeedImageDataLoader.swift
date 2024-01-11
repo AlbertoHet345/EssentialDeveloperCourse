73eec9c18fe5380c3c9144ff434a2e839cf0490a
Alberto García <albertopaul3@icloud.com>
2024-01-11 13:26:57 -0600

    Move `CoreDataFeedStore` implementation of the `FeedImageDataStore` protocol to production

---
+++ EssentialFeed/EssentialFeed/Feed Cache/Infrastructure/CoreData/CoreDataFeedStore+FeedImageDataLoader.swift
@@ -0,0 +1,18 @@
+//
+//  CoreDataFeedStore+FeedImageDataLoader.swift
+//  EssentialFeed
+//
+//  Created by Alberto Garcia Paul on 11/01/24.
+//
+
+import Foundation
+
+extension CoreDataFeedStore: FeedImageDataStore {
+    public func insert(_ data: Data, for url: URL, completion: @escaping (FeedImageDataStore.InsertionResult) -> Void) {
+        
+    }
+    
+    public func retrieve(dataForURL url: URL, completion: @escaping (FeedImageDataStore.RetrievalResult) -> Void) {
+        completion(.success(.none))
+    }
+}
