bf8904279c1e50bd8df5eb64ccba7d59802a269e
Alberto García <albertopaul3@icloud.com>
2024-03-15 10:17:07 -0600

    Inline param

--- EssentialFeed/EssentialFeedTests/Feed Presentation/FeedPresenterTests.swift
+++ EssentialFeed/EssentialFeedTests/Feed Presentation/FeedPresenterTests.swift
@@ -24,7 +24,8 @@ final class FeedPresenterTests: XCTestCase {
     
     // MARK: - Helpers
     
-    func localized(_ key: String, table: String = "Feed", file: StaticString = #filePath, line: UInt = #line) -> String {
+    func localized(_ key: String, file: StaticString = #filePath, line: UInt = #line) -> String {
+        let table = "Feed"
         let bundle = Bundle(for: FeedPresenter.self)
         let value = bundle.localizedString(forKey: key, value: nil, table: table)
         if value == key {
