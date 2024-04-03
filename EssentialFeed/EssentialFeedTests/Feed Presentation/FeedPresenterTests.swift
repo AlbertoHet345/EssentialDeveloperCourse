de5c61401b5de4a9eb9c3a0a5d5c6a26210f23e1
Alberto García <albertopaul3@icloud.com>
2024-04-03 12:44:11 -0600

    Remove unused code

--- EssentialFeed/EssentialFeedTests/Feed Presentation/FeedPresenterTests.swift
+++ EssentialFeed/EssentialFeedTests/Feed Presentation/FeedPresenterTests.swift
@@ -14,14 +14,6 @@ final class FeedPresenterTests: XCTestCase {
         XCTAssertEqual(FeedPresenter.title, localized("FEED_VIEW_TITLE"))
     }
     
-    func test_map_createsViewModel() {
-        let feed = uniqueImageFeed().model
-        
-        let viewModel = FeedPresenter.map(feed)
-        
-        XCTAssertEqual(viewModel.feed, feed)
-    }
-    
     // MARK: - Helpers
     
     func localized(_ key: String, file: StaticString = #filePath, line: UInt = #line) -> String {
