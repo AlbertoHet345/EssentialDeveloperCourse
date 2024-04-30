f2e8040e170f89fe6a37e677bed7325d28872b7e
Alberto García <albertopaul3@icloud.com>
2024-04-30 14:40:06 -0600

    Update snapshots with iPhone 15 Pro running iOS 17.0

--- EssentialFeed/EssentialFeediOSTests/Helpers/UIViewController+Snapshot.swift
+++ EssentialFeed/EssentialFeediOSTests/Helpers/UIViewController+Snapshot.swift
@@ -21,17 +21,18 @@ struct SnapshotConfiguration {
     
     static func iPhone(style: UIUserInterfaceStyle, contentSize: UIContentSizeCategory = .medium) -> SnapshotConfiguration {
         SnapshotConfiguration(
-            size: CGSize(width: 375, height: 667),
-            safeAreaInsets: UIEdgeInsets(top: 20, left: 0, bottom: 0, right: 0),
-            layoutMargins: UIEdgeInsets(top: 20, left: 16, bottom: 0, right: 16),
+            size: CGSize(width: 390, height: 844),
+            safeAreaInsets: UIEdgeInsets(top: 47, left: 0, bottom: 34, right: 0),
+            layoutMargins: UIEdgeInsets(top: 55, left: 8, bottom: 42, right: 8),
             traitCollection: UITraitCollection(traitsFrom: [
-                .init(forceTouchCapability: .available),
+                .init(forceTouchCapability: .unavailable),
                 .init(layoutDirection: .leftToRight),
                 .init(preferredContentSizeCategory: contentSize),
                 .init(userInterfaceIdiom: .phone),
                 .init(horizontalSizeClass: .compact),
                 .init(verticalSizeClass: .regular),
-                .init(displayScale: 2),
+                .init(displayScale: 3),
+                .init(accessibilityContrast: .normal),
                 .init(displayGamut: .P3),
                 .init(userInterfaceStyle: style)
             ]))
